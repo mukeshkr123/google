faef9504b26a252c342799c188ef4a19b60a5128
Mukesh Kumar <mkmehta2041@gmail.com>
2023-08-30 21:42:15 +0530

    added Home search

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,20 @@
 import HomeHeader from "@/components/HomeHeader";
+import Image from "next/image";
 import React from "react";
-
+import googleUrl from "../../utils/imageurl";
+import HomeSearch from "@/components/HomeSearch";
 const Page = () => {
   return (
     <>
       <HomeHeader />
+
+      {/* body
+       */}
+
+      <div className="flex flex-col items-center mt-24">
+        <Image width="300" src={googleUrl} height="200" />
+        <HomeSearch />
+      </div>
     </>
   );
 };
