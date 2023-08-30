3aecff0a094366d31140f0c27a399e65478b338e
Mukesh Kumar <mkmehta2041@gmail.com>
2023-08-30 21:49:06 +0530

    bug fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,6 @@
 import HomeHeader from "@/components/HomeHeader";
 import Image from "next/image";
 import React from "react";
-import googleUrl from "../../utils/imageurl";
 import HomeSearch from "@/components/HomeSearch";
 const Page = () => {
   return (
@@ -12,7 +11,12 @@ const Page = () => {
        */}
 
       <div className="flex flex-col items-center mt-24">
-        <Image width="300" src={googleUrl} height="200" />
+        <Image
+          width="300"
+          alt=""
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          height="200"
+        />
         <HomeSearch />
       </div>
     </>
